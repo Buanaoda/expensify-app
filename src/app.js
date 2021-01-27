5559bc2f305908fd4a6aeea5e83410f6dc5f3941
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-01-27 14:48:05 -0300

    Cleaned up some garbage

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'WaterBill', amount: 300, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'GasBill', amount: 200, createdAt: 2 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1000, createdAt: 3 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
