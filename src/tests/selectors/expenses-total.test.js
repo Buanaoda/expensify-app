5b14e4a3371c69365865fe4d1d8dd4fcb27b0332
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-01-29 13:07:57 -0300

    Added Expenses Summary component, ExpensesTotalValue selector and tests

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,20 @@
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../fixtures/expenses';
+import numeral from 'numeral'
+import "numeral/locales/pt-br";
+numeral.locale('pt-br');
+
+test('should return 0 if no expenses', () => {
+    const res = selectExpensesTotal([]);
+    expect(res).toBe(0);
+});
+
+test('should return correct value if one expense', () => {
+    const res = selectExpensesTotal([expenses[0]]);
+    expect(res).toBe(195);
+});
+
+test('should return correct value if multiple expense', () => {
+    const res = selectExpensesTotal(expenses);
+    expect(res).toBe(4500 + 109500 + 195);
+});
