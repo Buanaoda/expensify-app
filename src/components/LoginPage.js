ce8ab46863ff30fa369bb22bfa938c863b1bd0c3
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-03 17:00:28 -0300

    Setup login page and firebase Google Auth

---
+++ src/components/LoginPage.js
@@ -0,0 +1,17 @@
+/* JMJ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+  <div>
+    LoginPage
+    <button onClick={startLogin}>Login</button>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
