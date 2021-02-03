fae7d4e2843772f7ea65f4386f30de703aae9004
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-03 12:33:22 -0300

    Setup delete expense action for firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -40,6 +40,14 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
