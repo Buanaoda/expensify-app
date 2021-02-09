d26107bda84eeed8e12b916573e60cd7641e3fd0
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-09 13:14:15 -0300

    Edit page margin bottom fix

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -14,7 +14,7 @@ export class EditExpensePage extends React.Component {
   };
   render() {
     return (
-      <div>
+      <div className="list-body">
         <div className="page-header">
           <div className="content-container">
             <h1 className="page-header__title">Editar despesa</h1>
