c50973f1f2586850211a8df0171c3d9e1a1a8046
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-09 11:22:22 -0300

    added final styles and setup babel polyfill. Final deploy

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => (
+  <div className="loader">
+    <img className="loader__image" src="/images/duckLoading.gif"/>
+    <p>Olóh</p>
+  </div>
+);
+ 
+
+export default LoadingPage;
